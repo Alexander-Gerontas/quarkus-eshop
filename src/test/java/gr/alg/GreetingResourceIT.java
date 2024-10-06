66b98042787ee13733feee82bcf6ddc4919799f9
alexgerontas <alexandros.gerontas@datawise.ai>
2024-10-06 15:13:57 +0300

    project init

---
+++ src/test/java/gr/alg/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package gr.alg;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
